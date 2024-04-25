6e73f6ad0a34d8a2a286ef173799e11b594ca822
dllnnx <alenadenisova528@gmail.com>
2024-04-25 17:42:38 +0300

    lab7 creation

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -9,5 +9,5 @@ plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.5.0"
 }
 
-rootProject.name = "lab6"
+rootProject.name = "lab7"
 include("client", "server", "shared")
